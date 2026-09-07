b0ee862f741bc00fad125b77a7b2eabf43ce212c
mert-ucman <rati.be@hotmail.com>
2026-09-07 23:00:40 +0300

    cards added

--- src/assets/Components/Card.jsx
+++ src/assets/Components/Card.jsx
@@ -2,25 +2,27 @@
 
 
 
-function Card() {
+function Card({icon,iconColor,type,percentage,percentageColor,amount,time,iconStatus}) {
     return (
-        <div className="w-auto">
-            <div className="flex bg-[var(--bg-card)] rounded-[var(--rounded-xl)] border-[var(--border)] border-1 p-4 gap-x-4">
+        <div className="w-[33.33%] h-full">
+            <div className="h-full flex bg-[var(--bg-card)] rounded-[var(--rounded-xl)] border-[var(--border)] border-1 p-2 gap-x-2">
                 <div className="flex flex-col justify-between">
-                    <div className="p-2 rounded-xl h-12 w-12 xy-center bg-[var(--icon-bg-blue)]">
-                        <i className="fa-solid fa-wallet fa-xl text-white opacity-75"></i>
+                    <div className={`p-2 rounded-lg h-8 w-8 xy-center ${iconColor}`}>
+                        <i className={`${icon} text-white opacity-75`}></i>
                     </div>
-                    <div className="flex items-center gap-x-2 text-[var(--success)]">
-                        <i className="fa-solid fa-hammer fa-xs"></i>
-                        <p className="">%12</p>
+                    <div className={`flex items-center gap-x-0.5 ${percentageColor}`}>
+                        {
+                            iconStatus && (<i className="fa-solid fa-hammer fa-2xs"></i>)
+                        }
+                        <p className="text-[10px]">{percentage}</p>
                     </div>
                 </div>
                 <div className="flex flex-col justify-between">
                     <div className="">
-                        <h3 className="main-text-gray">Toplam <br />Harcama</h3>
-                        <p className="text-2xl font-bold">₺1.170</p>
+                        <h3 className="main-text-gray">{type}</h3>
+                        <p className="sub-text-white">{amount}</p>
                     </div>
-                    <p className="sub-text">Geçen haftaya göre</p>
+                    <p className="sub-text text-[8px]!">{time}</p>
                 </div>
             </div>
         </div>
--- src/assets/Components/TrackerWindow.jsx
+++ src/assets/Components/TrackerWindow.jsx
@@ -6,8 +6,8 @@ function TrackerWindow() {
         <div className="grid grid-cols-1 p-4 gap-y-4">
             <div className="flex justify-between items-center">
                 <div className="flex gap-x-3">
-                    <div className="p-2 rounded-[var(--rounded-xl)] h-14 w-14 xy-center bg-[var(--logo)]">
-                        <i className="fa-solid fa-chart-simple fa-xl text-white"></i>
+                    <div className="p-2 rounded-[var(--rounded-lg)] h-10 w-10 xy-center bg-[var(--logo)]">
+                        <i className="fa-solid fa-chart-simple fa-lg text-white"></i>
                     </div>
                     <div className="flex flex-col justify-center">
                         <h3 className="text-xl font-bold">SpendIt</h3>
@@ -15,8 +15,8 @@ function TrackerWindow() {
                     </div>
                 </div>
                 <div className="flex gap-x-3">
-                    <div className="xy-center">
-                        <i className="fa-regular fa-calendar fa-xl"></i>
+                    <div className="p-2 rounded-[var(--rounded-lg)] h-10 w-10 xy-center bg-[var(--bills)]">
+                        <i className="fa-regular fa-calendar fa-lg text-white"></i>
                     </div>
                     <div>
                         <h3 className="main-text">4 Eylül 2026</h3>
@@ -24,10 +24,35 @@ function TrackerWindow() {
                     </div>
                 </div>
             </div>
-            <div className="flex gap-x-4">
-                <Card />
-                <Card />
-                <Card />
+            <div className="flex gap-x-2 justify-between h-22">
+                <Card
+                    icon={`fa-solid fa-wallet fa-md`}
+                    iconColor={`bg-[var(--icon-bg-blue)]`}
+                    type={`Toplam Harcama`}
+                    percentageColor={`text-[var(--success)]`}
+                    amount={`₺1.170`}
+                    time={`Geçen haftaya göre`}
+                    percentage={`%12`}
+                    iconStatus={true}
+                />
+                <Card
+                    icon={`fa-solid fa-calendar fa-md`}
+                    iconColor={`bg-[var(--icon-bg-green)]`}
+                    type={`Bu Ay`}
+                    percentageColor={`text-[var(--danger)]`}
+                    amount={`₺3.250`}
+                    time={`Geçen aya göre`}
+                    percentage={`%8`}
+                    iconStatus={true}
+                />
+                <Card
+                    icon={`fa-solid fa-turkish-lira fa-md`}
+                    iconColor={`bg-[var(--icon-bg-purple)]`}
+                    type={`Harcama Sayısı`}
+                    amount={`12`}
+                    time={`Bu ay toplam`}
+                    iconStatus={false}
+                />
             </div>
         </div>
     )
