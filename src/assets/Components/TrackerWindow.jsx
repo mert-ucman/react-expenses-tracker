777eadbe1dd9483d6cca3ef46d16760be23122a5
mert-ucman <rati.be@hotmail.com>
2026-09-11 22:41:14 +0300

    harcamalarım eklendi

--- src/assets/Components/TrackerWindow.jsx
+++ src/assets/Components/TrackerWindow.jsx
@@ -30,7 +30,7 @@ function TrackerWindow() {
                     icon={
                         {
                             icon: `fa-solid fa-wallet fa-md`,
-                            bgColor:`bg-[var(--icon-bg-blue)]`,
+                            bgColor: `bg-[var(--icon-bg-blue)]`,
                             iconColor: `text-[var(--icon-blue)]`,
                             iconStatus: true,
                             rounded: `rounded-lg`
@@ -52,7 +52,7 @@ function TrackerWindow() {
                         {
                             icon: `fa-solid fa-calendar fa-md`,
                             iconColor: `text-[var(--icon-green)]`,
-                            bgColor:`bg-[var(--icon-bg-green)]`,
+                            bgColor: `bg-[var(--icon-bg-green)]`,
                             iconStatus: true,
                             rounded: `rounded-lg`
                         }
@@ -72,7 +72,7 @@ function TrackerWindow() {
                     icon={
                         {
                             icon: `fa-solid fa-turkish-lira fa-md`,
-                            bgColor:`bg-[var(--icon-bg-purple)]`,
+                            bgColor: `bg-[var(--icon-bg-purple)]`,
                             iconColor: `text-[var(--icon-purple)]`,
                             iconStatus: false,
                             rounded: `rounded-lg`
@@ -91,14 +91,119 @@ function TrackerWindow() {
                 />
             </div>
             <AddExpense />
-            <div className="flex flex-col">
-                <div >
-                    <div>
+            <div className="flex flex-col main-div">
+                <div className="flex justify-between border-b-1 pb-4 border-[var(--border)] mb-2">
+                    <div className="flex gap-x-2">
                         <IconWrapper
-                            icon={ `fa-solid fa-list-ul fa-md`}
+                            icon={`fa-solid fa-list-ul fa-md`}
                             color={`bg-[#242E40]`}
                             iconColor={`text-white`}
+                            rounded={`rounded-lg`}
                         />
+                        <div className="flex flex-col justify-between">
+                            <h3 className="main-text text-[12px]!">Harcamalarım</h3>
+                            <p className="sub-text text-[10px]!">Tüm harcamalarını gör.</p>
+                        </div>
+                    </div>
+                    <select name="expenseFilter" id="expenseFilter" className="expenseInput max-w-fit">
+                        <option value="all">Tümü</option>
+                        <option value="all">Beslenme</option>
+                        <option value="all">Eğlence</option>
+                        <option value="all">Ulaşım</option>
+                        <option value="all">Eğitim</option>
+                        <option value="all">Yaşam&Sağlık</option>
+                    </select>
+                </div>
+                <div className="">
+                    <div className="flex justify-between border-b border-[var(--border)] py-1">
+                        <div className="flex gap-x-2">
+                            <IconWrapper
+                                icon={`fa-solid fa-coffee fa-sm`}
+                                color={`bg-[#55463A]`}
+                                iconColor={`text-white`}
+                                rounded={`rounded-full`}
+                            />
+                            <div className="flex flex-col justify-between">
+                                <h3 className="main-text text-[12px]!">Kahve</h3>
+                                <p className="sub-text text-[10px]!">4 Eylül 2026</p>
+                            </div>
+                        </div>
+                        <div className="flex xy-center gap-x-4">
+                            <p className="text-[#F16D71] font-bold">₺120</p>
+                            <i className="fa-solid fa-chevron-right fa-xs text-gray-500"></i>
+                        </div>
+                    </div>
+                    <div className="flex justify-between border-b border-[var(--border)] py-1">
+                        <div className="flex gap-x-2">
+                            <IconWrapper
+                                icon={`fa-solid fa-bag-shopping fa-sm`}
+                                color={`bg-[#1D6750]`}
+                                iconColor={`text-white`}
+                                rounded={`rounded-full`}
+                            />
+                            <div className="flex flex-col justify-between">
+                                <h3 className="main-text text-[12px]!">Market</h3>
+                                <p className="sub-text text-[10px]!">3 Eylül 2026</p>
+                            </div>
+                        </div>
+                        <div className="flex xy-center gap-x-4">
+                            <p className="text-[#F16D71] font-bold">₺850</p>
+                            <i className="fa-solid fa-chevron-right fa-xs text-gray-500"></i>
+                        </div>
+                    </div>
+                    <div className="flex justify-between border-b border-[var(--border)] py-1">
+                        <div className="flex gap-x-2">
+                            <IconWrapper
+                                icon={`fa-solid fa-tv fa-sm`}
+                                color={`bg-[#46408B]`}
+                                iconColor={`text-white`}
+                                rounded={`rounded-full`}
+                            />
+                            <div className="flex flex-col justify-between">
+                                <h3 className="main-text text-[12px]!">Netflix</h3>
+                                <p className="sub-text text-[10px]!">2 Eylül 2026</p>
+                            </div>
+                        </div>
+                        <div className="flex xy-center gap-x-4">
+                            <p className="text-[#F16D71] font-bold">₺200</p>
+                            <i className="fa-solid fa-chevron-right fa-xs text-gray-500"></i>
+                        </div>
+                    </div>
+                    <div className="flex justify-between border-b border-[var(--border)] py-1">
+                        <div className="flex gap-x-2">
+                            <IconWrapper
+                                icon={`fa-solid fa-bus fa-sm`}
+                                color={`bg-[#1B416F]`}
+                                iconColor={`text-white`}
+                                rounded={`rounded-full`}
+                            />
+                            <div className="flex flex-col justify-between">
+                                <h3 className="main-text text-[12px]!">Ulaşım</h3>
+                                <p className="sub-text text-[10px]!">1 Eylül 2026</p>
+                            </div>
+                        </div>
+                        <div className="flex xy-center gap-x-4">
+                            <p className="text-[#F16D71] font-bold">₺40</p>
+                            <i className="fa-solid fa-chevron-right fa-xs text-gray-500"></i>
+                        </div>
+                    </div>
+                    <div className="flex justify-between py-1">
+                        <div className="flex gap-x-2">
+                            <IconWrapper
+                                icon={`fa-solid fa-dna fa-sm`}
+                                color={`bg-[#A7434F]`}
+                                iconColor={`text-white`}
+                                rounded={`rounded-full`}
+                            />
+                            <div className="flex flex-col justify-between">
+                                <h3 className="main-text text-[12px]!">Sağlık</h3>
+                                <p className="sub-text text-[10px]!">31 Ağustos 2026</p>
+                            </div>
+                        </div>
+                        <div className="flex xy-center gap-x-4">
+                            <p className="text-[#F16D71] font-bold">₺300</p>
+                            <i className="fa-solid fa-chevron-right fa-xs text-gray-500"></i>
+                        </div>
                     </div>
                 </div>
             </div>
