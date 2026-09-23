7e875dc93b414977c6a3931dabfa4986300db054
Mert Uçman <rati.be@hotmail.com>
2026-09-23 17:12:38 +0300

    mantık tekrarlandı davranışlara geçeceğiz

--- src/assets/Context/ExpenseContext.jsx
+++ src/assets/Context/ExpenseContext.jsx
@@ -4,12 +4,12 @@ import expenseReducer from "../Reducers/expenseReducer";
 
 const ExpenseContext = createContext();
 const initialState = {
-    expenses:{
-        id:0,
-        title:"",
-        amount:0,
-        category:"",
-        date:""
+    expenses: {
+        id: 0,
+        title: "",
+        amount: 0,
+        category: "",
+        date: ""
     }
 };
 
@@ -21,8 +21,8 @@ function ExpenseContextProvider({ children }) {
         <ExpenseContext.Provider value={{ state, dispatch }}>
 
 
-        
-        {children}
+
+            {children}
 
         </ExpenseContext.Provider>
     )
@@ -31,3 +31,37 @@ function ExpenseContextProvider({ children }) {
 }
 
 export default ExpenseContextProvider;
+
+// Uygulamanın verisi başlangıçta ne? → initialState
+
+//expenseReducer'daki olaylar gerçekleştiğinde return edilen duruma göre state dönecek o state'i de state'e atacak
+//Yeni React'a şunu diyoruz, Benim bir state'im olacak (expenseReducer'dan return edilen). Bu state'in nasıl değişeceğine expenseReducer karar verecek ve başlangıç değeri de initialState olacak.
+
+//state → mevcut veri
+//dispatch → state'in değiştirilmesini istemek için kullanılacak fonksiyon
+//expenseReducer → Tamam ne yapılması istendi ? buna göre state'i nasıl değiştireceğim ? diye karar veren fonksiyon
+
+//ExpenseContext.Provider ise state ve dispatch'i alır ve altındaki children'a (children'i de ana function ile parametre olarak aldı)'a dağıtıma açıyorum
+
+
+//ekle sil güncelle için tek tek state açabiliriz ama proje büyüdükçe bunları zor yönetiriz o yüzden reducer'a ihtiyaç duyuyoruz.
+
+//dispatch içerisinde payload gönderdiğin nesne yerine geçer.
+
+/**
+action: Reducer'a gönderilen eylem nesnesi.
+
+action.payload: Eylemin taşıdığı harcama nesnesi.
+
+action.payload.amount: Bu harcama nesnesinin amount özelliği.
+ */
+
+/*
+Backend → Harcama verilerini getirir
+         ↓
+React → Verileri state içinde tutar
+         ↓
+Reducer → Ekleme, silme, güncelleme işlemlerini yönetir
+         ↓
+Context → Verileri bileşenlere dağıtır
+*/
